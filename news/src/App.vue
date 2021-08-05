3010c1584d43174a1e992656f8e34e6702940542
BrianH <brianhwang29@gmail.com>
2021-08-05 18:00:20 +0900

    initial commit

---
+++ news/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <!-- 
+      url이 만약 jobs 일경우 
+      <JobsView></JobsView>
+      형식으로 됨
+    -->
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
